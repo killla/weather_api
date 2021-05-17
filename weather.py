02e3f9523eb0b93ad6df1725f65d803d8fccc298
killla <killa.granet@gmail.com>
2021-05-17 17:42:35 +0500

    Release weather forecast

---
+++ weather.py
@@ -0,0 +1,14 @@
+import requests
+
+url_template = 'http://wttr.in/{}?nTqum&lang=ru'
+payload = {'Tqum':'', 'lang':'ru'}
+locations = ['London', 'svo', 'Череповец']
+
+for location in locations:
+    try:
+        url = url_template.format(location)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.HTTPError as error:
+        exit("Can't get data from server:\n{0}".format(error))
